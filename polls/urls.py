acc482749755da2ab0e3f973180d603558ed2c0c
Reza <example email>
2023-10-14 20:10:22 +0330

    add detail & result & vote views

--- polls/urls.py
+++ polls/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import index
+from .views import index, detail, vote, result
 
 urlpatterns = [
     path('', index),
+    path("<int:question_id>/", detail),
+    path("<int:question_id>/result/", result),
+    path("<int:question_id>/vote/", vote),
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,8 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Question
+from django.template import loader
 
 
 def index(request):
-    return HttpResponse('Hello world !')
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    template = loader.get_template('polls/index.html')
+    context = {"latest_question_list": latest_question_list}
+    return HttpResponse(template.render(context, request))
+
+
+def detail(request, question_id):
+    return HttpResponse(f"You are looking at question {question_id}")
+
+
+def result(request, question_id):
+    return HttpResponse(f"You are looking at the results of question {question_id}")
+
+
+def vote(request, question_id):
+    return HttpResponse(f"You are voting on question {question_id}")
 
 
